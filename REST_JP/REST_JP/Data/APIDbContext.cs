736b70cde1b5e24cfb474cb0a27d0961ea777c20
Jack <jackpatterson.10.02@gmail.com>
2022-10-12 11:44:29 +0100

    REST API Started

---
+++ REST_JP/REST_JP/Data/APIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_JP.Data
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Account> userRequests { get; set; }
+        public DbSet<Transaction> transactionRequests { get; set; }
+    }
+}
