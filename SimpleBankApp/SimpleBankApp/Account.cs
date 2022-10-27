8a628b9df2f92ce9107d72aa58ee9abc02d6e5fb
HydenJohnson75 <78604065+ToxicOddesey@users.noreply.github.com>
2022-10-27 16:04:36 +0100

    Added Login form code
    Signed-off-by: HydenJohnson75 <78604065+ToxicOddesey@users.noreply.github.com>

---
+++ SimpleBankApp/SimpleBankApp/Account.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBankApp
+{
+    public class Account
+    {
+
+        int accountID;
+        string accountName;
+        string iban;
+        int pac;
+        float balance;
+
+        public Account()
+        {
+
+        }
+
+        public Account(int accountID, string accountName, string iban, int pac, float balance)
+        {
+            this.accountID = accountID;
+            this.accountName = accountName;
+            this.iban = iban;
+            this.pac = pac;
+            this.balance = balance;
+        }
+
+        public int getAccountID()
+        {
+            return accountID;
+        }
+
+        public string getAccountName()
+        {
+            return accountName;
+        }
+
+        public string getIban()
+        {
+            return iban;
+        }
+
+        public int getPac()
+        {
+            return pac;
+        }
+
+        public float getBalance()
+        {
+            return balance;
+        }
+
+        public void setAccountId(int customerAccountID)
+        {
+            accountID = customerAccountID;
+        }
+
+        public void setAccountName(string customerAccountName)
+        {
+            accountName = customerAccountName;
+        }
+
+        public void setIban(string customerIban)
+        {
+            iban = customerIban;
+        }
+
+        public void setPac(int customerPac)
+        {
+            pac = customerPac;
+        }
+
+        public void setBalance(float customerBalance)
+        {
+            balance = customerBalance;
+        }
+
+    }
+}
---
+++ SimpleBankApp/SimpleBankApp/Frm_AccountMainPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleBankApp
+{
+    public partial class Frm_AccountMainPage : Form
+    {
+        private Frm_Login Parentform;
+        private Account UserAccount;
+
+        public Frm_AccountMainPage()
+        {
+            InitializeComponent();
+        }
+
+        public Frm_AccountMainPage(Frm_Login parent, Account userAccount)
+        {
+            InitializeComponent();
+            Parentform = parent;    
+            UserAccount = userAccount;  
+        }
+    }
+}
